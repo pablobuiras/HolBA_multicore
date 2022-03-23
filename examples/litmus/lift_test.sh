f15c341b563c6e558224afcd1c1da691aee8be08
Henrik Karlsson <hakarlsson@mailbox.org>
2022-03-23 12:04:54 +0100

    Relifts litmus tests.
    Affected tests are tests with AMO operations and LR/SC.

--- examples/litmus/lift_test.sh
+++ examples/litmus/lift_test.sh
@@ -3,6 +3,19 @@
 LITMUS=${1}.litmus
 JSON=${1}.json
 
-if [ ! -f $JSON ]; then
-        ./lift_test $LITMUS $JSON
+if [ -f $JSON ]; then
+        echo "${1}: Skipping."
+        exit 1
+fi
+if [ ! -f $LITMUS ]; then
+        echo "${1}: Missing litmus file."
+        exit 2
+fi
+./lift_test $LITMUS $JSON >/dev/null 2>&1
+
+if [ -f $JSON ]; then
+        echo "${1}: Lifted."
+else
+        echo "${1}: Lift failed."
+        exit 3
 fi
